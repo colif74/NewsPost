0923d0675a3e76f121024fb92af901ac8837e65f
colif74 <128209514+colif74@users.noreply.github.com>
2023-05-19 14:33:33 +0500

    Add files via upload

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -9,40 +9,39 @@ from django.db.models import Sum
 
 class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    email = models.EmailField()
+    email = models.EmailField(default=True)
     rating = models.IntegerField(default=0)
 
     def update_rating(self):
-        author_rating_post = Post.objects.filter(author_id=self.pk).aggregate(post_rating_sum=Coalesce(Sum('rating_post') * 3, 0))
-        author_rating_comment = Comment.objects.filter(user_id=self.user).aggregate(comment_rating_sum=Coalesce(Sum('rating_comment'), 0))
-        author_rating_comment_post = Comment.objects.filter(post__author__id=self.user).aggregate(post_comment_rating_sum=Coalesce(Sum('rating_comment'), 0))
-        self.rating = author_rating_post['post_rating_sum'] + author_rating_comment['comment_rating_sum'] + author_rating_comment_post['post_comment_rating_sum']
+        author_rating_post = Post.objects.filter(author_id=self.pk).\
+            aggregate(post_rating_sum=Coalesce(Sum('rating_post') * 3, 0))
+        author_rating_comment = Comment.objects.filter(user_id=self.user).\
+            aggregate(comment_rating_sum=Coalesce(Sum('rating_comment'), 0))
+        author_rating_comment_post = Comment.objects.filter(post__author__id=self.user).\
+            aggregate(post_comment_rating_sum=Coalesce(Sum('rating_comment'), 0))
+        self.rating = author_rating_post['post_rating_sum'] + author_rating_comment['comment_rating_sum'] \
+            + author_rating_comment_post['post_comment_rating_sum']
         self.save()
-
-economy = 'EC'
-politician = 'PL'
-worldnews = 'WN'
-hostnews = 'HN'
-sport = 'SP'
-
-POSITION = [
-            (economy, 'Экономика'),
-            (politician, 'Политика'),
-            (worldnews, 'Мировые новости'),
-            (hostnews, 'Местные новости'),
-            (sport, 'Спортивные новости')
-        ]
 
 
 class Category(models.Model):
-       post_or_news = models.CharField(max_length=4, help_text="news" or "post")
-       category = models.CharField(max_length=2, choices=POSITION, default='HN')
+    POSITION = [
+        ('economy', 'Экономика'),
+        ('politician', 'Политика'),
+        ('world news', 'Мировые новости'),
+        ('host news', 'Местные новости'),
+        ('sport', 'Спортивные новости')
+    ]
+
+    post_or_news = models.CharField(max_length=4, help_text="news" or "post")
+    category = models.CharField(max_length=15, choices=POSITION, default='host news')
+
 
 class Post(models.Model):
     objects = None
     date_in = models.DateField(auto_now_add=True)
     header = models.CharField(max_length=255)
-    contents = models.TextField()
+    contents = models.TextField(blank=True)
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
     category = models.ManyToManyField(Category, through='PostCategory')
     rating_post = models.IntegerField(default=0)
@@ -55,10 +54,8 @@ class Post(models.Model):
         self.rating_post -= 1
         self.save()
 
-
     def preview(self):
         return f"{self.contents[:124]}..."
-
 
 
 class PostCategory(models.Model):
@@ -68,8 +65,8 @@ class PostCategory(models.Model):
 
 class Comment(models.Model):
     objects = None
-    text_comment = models.TextField()
-    date_creat = models.DateField(auto_now_add=True)
+    text_comment = models.TextField(blank=True)
+    date_in = models.DateField(auto_now_add=True)
     rating_comment = models.IntegerField(default=0)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
@@ -78,49 +75,5 @@ class Comment(models.Model):
         self.save()
 
     def dislike(self):
-         self.rating_comment -= 1
-         self.save()
-
-
-# class LikeDislike(models.Model):
-#     LIKE = 1
-#     DISLIKE = -1
-#
-#     LIST = (
-#         (LIKE, 'нравится'),
-#         (DISLIKE, 'не нравится')
-#     )
-#
-#     znak = models.SmallIntegerField(verbose_name=('голос'), choices=LIST)
-#     users = models.ForeignKey(User, verbose_name=('Пользователь'), on_delete=models.CASCADE)
-#
-#     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
-#     object_id = models.PositiveIntegerField()
-#     content_object = GenericForeignKey()
-#
-#
-# class LikeDislikeManager(models.Manager):
-#     use_for_related_fields = True
-#
-#     def likes(self):
-#         return self.get_queryset().filter(znak__gt=0)
-#
-#     def dislike(self):
-#         return self.get_queryset().filter(znak__lt=0)
-#
-#     def sum_rating(self):
-#         return self.get_queryset().aggregate(Sum('znak')).get('znak__sum') or 0
-#
-#     def articles(self):
-#         return self.get_queryset().filter(content_type__model='article').order_by('-articles__pub_date')
-#
-#     def comments(self):
-#         return self.get_queryset().filter(content_type__model='comment').order_by('-comments__pub_date')
-#
-# class Article(models.Model):
-#         votes = GenericRelation(LikeDislike, related_query_name='articles')
-#
-# class Coment(models.Model):
-#     votes = GenericRelation(LikeDislike, related_query_name='comments')
-#
-
+        self.rating_comment -= 1
+        self.save()
