d67d5dde8b324c963f79c195482debb5c5f8bd76
colif74 <128209514+colif74@users.noreply.github.com>
2023-05-27 15:03:22 +0500

    Delete NewsPaper directory

--- NewsPaper/news/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import PostList, CategoryList, AuthorList
-
-
-urlpatterns = [path('', PostList.as_view(), name='posts'),
-               path('author', AuthorList.as_view(), name='author'),
-               path('category', CategoryList.as_view(), name='category'),
-               ]
