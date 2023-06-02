d5dace5dd5f39ec79edf591b3bf011c60e2028ef
colif74 <128209514+colif74@users.noreply.github.com>
2023-06-02 10:11:27 +0500

    Add files via upload

---
+++ NewsPaper/news/forms.py
@@ -0,0 +1,48 @@
+from django import forms
+from django.core.exceptions import ValidationError
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    header = forms.CharField(min_length=10)
+
+    class Meta:
+        model = Post
+        fields = [
+                'post_tip',
+                'category',
+                'header',
+                'contents',
+                'author',
+                    ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        header = cleaned_data.get('header')
+        contents = cleaned_data.get("contents")
+        if contents == header:
+            raise ValidationError({"Содержание должно отлтчатся от заголовка"})
+        return cleaned_data
+
+
+class StateForm(forms.ModelForm):
+    header = forms.CharField(min_length=10)
+
+    class Meta:
+        model = Post
+        fields = [
+                'post_tip',
+                'category',
+                'header',
+                'contents',
+                'author',
+                    ]
+
+    def clean(self):
+        cleaned_data = super().clean()
+        header = cleaned_data.get('header')
+        contents = cleaned_data.get("contents")
+        if contents == header:
+            raise ValidationError({"Содержание должно отлтчатся от заголовка"})
+        return cleaned_data
