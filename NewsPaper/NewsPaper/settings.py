a3f4c5611448968ddeeac415bd775bd85ba5e3e0
colif74 <128209514+colif74@users.noreply.github.com>
2023-06-07 15:48:51 +0500

    Update settings.py

--- NewsPaper/NewsPaper/settings.py
+++ NewsPaper/NewsPaper/settings.py
@@ -155,4 +155,4 @@ ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USERNAME_REQUIRED = False
 ACCOUNT_AUTHENTICATION_METHOD = 'email'
 ACCOUNT_EMAIL_VERIFICATION = 'none'
-ACCOUNT_FORMS = {'signup': 'sign.models.BasicSignupForm'}
+ACCOUNT_FORMS = {'signup': 'news.models.BasicSignupForm'}
