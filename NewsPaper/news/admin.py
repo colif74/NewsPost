bdfb98bdb34c6c78ca98f1d70a6318f207ded61d
colif74 <128209514+colif74@users.noreply.github.com>
2023-06-10 17:07:18 +0500

    Delete NewsPaper directory

--- NewsPaper/news/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Author, Post, Category
-
-admin.site.register(Author)
-admin.site.register(Post)
-admin.site.register(Category)
-
-# Register your models here.
