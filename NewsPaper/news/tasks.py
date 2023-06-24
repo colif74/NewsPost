3718fbb621e3c7ae55c2701740fd662f1cd4f9b5
colif74 <128209514+colif74@users.noreply.github.com>
2023-06-24 09:01:15 +0500

    Delete NewsPaper directory

--- NewsPaper/news/tasks.py
+++
@@ -1,9 +0,0 @@
-from celery import shared_task
-from datetime import datetime, timedelta
-from .models import Post
-from .signals import send_notifications
-
-@shared_task
-def frech_news():
-    frech_post = Post.objects.all().apply_async(eta = datetime.now() - timedelta(days=7))
-    frech_post
